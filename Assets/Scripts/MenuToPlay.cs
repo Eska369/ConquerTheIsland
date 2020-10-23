edb42685713f1cea3d6af28116f79a2e49b15725
Marcin <marcin8eska@gmail.com>
2020-10-23 13:04:09 +0200

    Update

---
+++ Assets/Scripts/MenuToPlay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class MenuToPlay : MonoBehaviour
+{
+    public void Scene1To3()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
