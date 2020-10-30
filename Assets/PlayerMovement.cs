ca0fd6eb4d90401c7632037a1bd325d68b0d09bf
Marcin <marcin8eska@gmail.com>
2020-10-30 10:34:47 +0100

    Poprawiona wielkosc postaci oraz tla, collidery na ziemi, galaz, chest i dach

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+    }
+
+    private void FixedUpdate()
+    {
+        //Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
