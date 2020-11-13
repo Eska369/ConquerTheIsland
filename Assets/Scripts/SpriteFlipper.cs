a1428e0ae33a7d84df08dbbfec15c57c37cbc5dc
Marcin <marcin8eska@gmail.com>
2020-11-13 10:57:43 +0100

    Dodac warunki w animatorze

---
+++ Assets/Scripts/SpriteFlipper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFlipper : MonoBehaviour
+{
+    private SpriteRenderer mySpriteRenderer;
+
+
+    private void Awake()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // if the A key was pressed this frame
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            // if the variable isn't empty (we have a reference to our SpriteRenderer
+            if (mySpriteRenderer != null)
+            {
+                // flip the sprite
+                mySpriteRenderer.flipX = true;
+            }
+        }
+    }
+}
